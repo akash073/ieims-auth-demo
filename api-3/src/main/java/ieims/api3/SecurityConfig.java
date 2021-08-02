a9f623fa6a2072535aed4fc7362d18c37e60d81f
SyedAkash <mahmud.hasan@dsinnovators.com>
2021-08-02 10:10:56 +0600

    CSRF and session related code are removed

--- api-3/src/main/java/ieims/api3/SecurityConfig.java
+++ api-3/src/main/java/ieims/api3/SecurityConfig.java
@@ -43,8 +43,6 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http
-                .csrf()
-                .disable()
                 .authorizeRequests().anyRequest().permitAll()
                 .and()
                 .oauth2Login()
